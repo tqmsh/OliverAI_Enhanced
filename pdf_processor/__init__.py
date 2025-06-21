cba7d549546f4d42fdc774766faa5d644b2a923f
MoonGlade-create <steven626226@gmail.com>
2025-06-20 22:44:31 -0400

    POC

---
+++ pdf_processor/__init__.py
@@ -0,0 +1,7 @@
+"""
+PDF to Markdown Converter
+
+Simple service for converting PDFs to Markdown using Marker.
+"""
+
+__version__ = "1.0.0" 
---
+++ pdf_processor/services/__init__.py
@@ -0,0 +1,15 @@
+"""
+PDF Processor Services
+
+Simple PDF conversion service using official Marker pattern.
+"""
+
+from .pdf_conversion_service import (
+    PDFConversionService,
+    ConversionResult
+)
+
+__all__ = [
+    "PDFConversionService",
+    "ConversionResult"
+] 
